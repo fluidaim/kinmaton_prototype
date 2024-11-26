42b833088d159e703ed88e4e77e79df9601f4200
fluidaim <jhoh@JongHwans-MacBook-Air.local>
2024-11-26 15:17:03 +0900

    fullscreen

--- sketch.js
+++ sketch.js
@@ -18,23 +18,21 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(400, 600);
+  createCanvas(windowWidth, windowHeight);
   
-  // 버튼 생성
+  // 버튼 생성 및 중앙 정렬
   button = createButton('점괘 뽑기');
-  button.position(width/2 - 50, height - 100);
+  button.position(windowWidth/2 - 50, windowHeight - 100);
   button.mousePressed(drawFortune);
   button.class('fortune-button');
   
-  // 텍스트 설정
   textAlign(CENTER, CENTER);
-  textSize(16);
+  textSize(min(windowWidth, windowHeight) * 0.03); // 화면 크기에 비례한 텍스트 크기
 }
 
 function draw() {
   background(245);
   
-  // 점쟁이 이미지 표시
   imageMode(CENTER);
   
   // 화면 흔들기 효과
@@ -47,17 +45,18 @@ function draw() {
     }
   }
   
-  // 점쟁이 이미지 그리기
-  image(fortuneTeller, width/2, height/3, 200, 300);
+  // 이미지 크기를 화면 크기에 비례하게 조정
+  let imgWidth = min(windowWidth, windowHeight) * 0.4;
+  let imgHeight = imgWidth * 1.5;
+  image(fortuneTeller, windowWidth/2, windowHeight/3, imgWidth, imgHeight);
   
-  // 점괘 텍스트 표시 (페이드 인 효과)
   if (fortune !== '') {
     if (fadeIn < 255) {
       fadeIn += 5;
     }
     
     fill(0, 0, 0, fadeIn);
-    text(fortune, width/2, height/2 + 100);
+    text(fortune, windowWidth/2, windowHeight/2 + imgHeight/2);
   }
 }
 
@@ -74,4 +73,11 @@ function drawFortune() {
 
   // 진동 효과
   navigator.vibrate([100, 30, 100, 30]);
+}
+
+// 윈도우 크기 변경 시 캔버스 리사이즈
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  button.position(windowWidth/2 - 50, windowHeight - 100);
+  textSize(min(windowWidth, windowHeight) * 0.03);
 }
